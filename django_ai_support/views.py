04f954c56712ca18fa2ef344e747306eda30f874
EmadDeve20 <emaddeve20@gmail.com>
2025-10-28 20:19:47 +0330

    improve views and add view with authenticated for chat

--- django_ai_support/views.py
+++ django_ai_support/views.py
@@ -1,12 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from drf_spectacular.utils import extend_schema
 
 from .serializers import InputOutputChatSerializer
 from .backends import normal_chat_with_ai
 
-class ChatAiSupportApi(APIView):
+class BaseApi(APIView):
+
+    @property
+    def thread_id(self):
+        return self.request.session.session_key
+
+
+class ChatAiSupportApi(BaseApi):
 
     @extend_schema(
         tags=["Django AI Support"],
@@ -19,8 +27,17 @@ class ChatAiSupportApi(APIView):
         serializer.is_valid(raise_exception=True)
 
         ai_response = normal_chat_with_ai(serializer.validated_data["message"],
-                                          session_id=request.session.session_key)
+                                          thread_id=self.thread_id)
         
         return Response(InputOutputChatSerializer({"message": ai_response}).data)
 
+
+
+class AuthChatAiSupportApi(ChatAiSupportApi):
+
+    permission_classes = (IsAuthenticated,)
+
+    @property
+    def thread_id(self):
+        return self.request.user.id
 
