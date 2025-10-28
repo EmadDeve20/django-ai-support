66bda4fcee5e5b635a12febc93b903ee5769006c
EmadDeve20 <emaddeve20@gmail.com>
2025-10-28 20:18:51 +0330

    Update chat method to get thread_id

--- django_ai_support/backends.py
+++ django_ai_support/backends.py
@@ -2,9 +2,19 @@ from langchain_core.messages import HumanMessage
 
 from .graphs import compiled_graph
 
-def normal_chat_with_ai(user_message:str, session_id:str) -> str:
+def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
+    """
+    function to chat with AI
 
-    config = {"configurable": {"thread_id": session_id}}
+    Args:
+        user_message (str): message of user
+        thread_id (str): id of threade to save messages
+
+    Returns:
+        str: return comming message
+    """
+
+    config = {"configurable": {"thread_id": str(thread_id)}}
 
     messages = [
         HumanMessage(user_message)
