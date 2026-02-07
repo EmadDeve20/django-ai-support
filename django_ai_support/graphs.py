aa13b509d9465d48e15b7bbce3f6212709ed6615
EmadDeve20 <emaddeve20@gmail.com>
2026-02-07 20:45:45 +0330

    use DBs to save short-term memory and create init function for main agent

--- django_ai_support/graphs.py
+++ django_ai_support/graphs.py
@@ -1,7 +1,12 @@
 from langgraph.graph import START, END, MessagesState, StateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.checkpoint.memory import MemorySaver 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.redis import RedisSaver
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.mongodb import MongoDBSaver
+
 
 from .settings import api_settings
 
@@ -16,34 +21,87 @@ if api_settings.SYSTEM_PROMPT:
 else:
     system_prompt_template = None
 
-def chat_node(state:MessagesState):
+
+def init_agent():
+    """
+    initialize agent
+    """
+
+    def chat_node(state:MessagesState):
+        
+        if system_prompt_template:
+            messages = system_prompt_template.invoke(state)
+        else:
+            messages = state["messages"]
+
+        response = api_settings.LLM_MODEL.invoke(messages)
+
+        return {"messages": response}
+
+
+    def short_term_memory_from_settings():
+        
+        memory_saver = None
+
+        # TODO: add Validation to check memory saver is None or not
+        if api_settings.SHORT_TERM_MEMORY.type.lower() == "redis":
+            memory_saver = RedisSaver
+        
+        elif api_settings.SHORT_TERM_MEMORY.type.lower() == "postgres":
+            memory_saver = PostgresSaver
+        
+        elif api_settings.SHORT_TERM_MEMORY.type.lower() == "mongodb":
+            memory_saver = MongoDBSaver
+
+        with memory_saver.from_conn_string(api_settings.SHORT_TERM_MEMORY.url) as checkpointer:
+
+            tool_node = ToolNode(api_settings.TOOLS)
+
+            graph = StateGraph(MessagesState)
+            graph.add_node("chat", chat_node)
+            graph.add_node("tools", tool_node)
+
+            graph.add_conditional_edges(
+                "chat",
+                tools_condition
+            )
+
+            graph.add_edge(START, "chat")
+            graph.add_edge("tools", "chat")
+            graph.add_edge("chat", END)
+
+            compiled_graph = graph.compile(checkpointer=checkpointer)
+
+            return compiled_graph
     
-    if system_prompt_template:
-        messages = system_prompt_template.invoke(state)
-    else:
-        messages = state["messages"]
 
-    response = api_settings.LLM_MODEL.invoke(messages)
+    def default_short_term_memory():
 
-    return {"messages": response}
+        tool_node = ToolNode(api_settings.TOOLS)
+
+        memory = MemorySaver()
+
+        graph = StateGraph(MessagesState)
+        graph.add_node("chat", chat_node)
+        graph.add_node("tools", tool_node)
+
+        graph.add_conditional_edges(
+            "chat",
+            tools_condition
+        )
+
+        graph.add_edge(START, "chat")
+        graph.add_edge("tools", "chat")
+        graph.add_edge("chat", END)
+
+        compiled_graph = graph.compile(checkpointer=memory)
+
+        return compiled_graph
 
 
-tool_node = ToolNode(api_settings.TOOLS)
+    if api_settings.SHORT_TERM_MEMORY is None:
+        return default_short_term_memory()
+    
+    return short_term_memory_from_settings()
 
-memory = MemorySaver()
-
-graph = StateGraph(MessagesState)
-graph.add_node("chat", chat_node)
-graph.add_node("tools", tool_node)
-
-graph.add_conditional_edges(
-    "chat",
-    tools_condition
-)
-
-graph.add_edge(START, "chat")
-graph.add_edge("tools", "chat")
-graph.add_edge("chat", END)
-
-compiled_graph = graph.compile(checkpointer=memory)
 
