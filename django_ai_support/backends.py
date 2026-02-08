60ae68887c5b7ba25acd764604dacae9dd8bb822
EmadDeve20 <emaddeve20@gmail.com>
2026-02-09 00:02:17 +0330

    update exception handlers to add mongodb in use invoke graph

--- django_ai_support/backends.py
+++ django_ai_support/backends.py
@@ -1,7 +1,9 @@
 from redis.exceptions import ConnectionError as RedisConnectionError
+from psycopg2 import OperationalError as PostgresConnectionError
+from pymongo.errors import ConnectionFailure as MongodbConnectionError
+from pymongo.errors import ServerSelectionTimeoutError
 
 from django.utils.translation import gettext_lazy as _
-
 from rest_framework.exceptions import ValidationError
 
 from langchain_core.messages import HumanMessage
@@ -30,7 +32,20 @@ def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
 
     try:
         ai_response = compiled_graph.invoke({"messages": messages}, config=config)
+
     except RedisConnectionError:
+        raise ValidationError(_("AI support is not available right now!"))
+
+    except PostgresConnectionError:
+        raise ValidationError(_("AI support is not available right now!"))
+
+    except MongodbConnectionError:
+        raise ValidationError(_("AI support is not available right now!"))
+
+    except ServerSelectionTimeoutError:
+        raise ValidationError(_("AI support is not available right now!"))
+
+    except Exception:
         raise ValidationError(_("AI support is not available right now!"))
 
     ai_response = ai_response["messages"][-1].content
