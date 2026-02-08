0f9e4719abf84192cc26984984c324d97d1f2466
EmadDeve20 <emaddeve20@gmail.com>
2026-02-08 23:36:22 +0330

    Update to use compilde graph and handle connection exception of redis

--- django_ai_support/backends.py
+++ django_ai_support/backends.py
@@ -1,6 +1,13 @@
+from redis.exceptions import ConnectionError as RedisConnectionError
+
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.exceptions import ValidationError
+
 from langchain_core.messages import HumanMessage
 
-from .graphs import init_agent
+
+from .graphs import compiled_graph
 
 def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
     """
@@ -14,7 +21,6 @@ def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
         str: return coming message
     """
 
-    agent = init_agent()
 
     config = {"configurable": {"thread_id": str(thread_id)}}
 
@@ -22,7 +28,10 @@ def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
         HumanMessage(user_message)
     ]
 
-    ai_response = agent.invoke({"messages": messages}, config=config)
+    try:
+        ai_response = compiled_graph.invoke({"messages": messages}, config=config)
+    except RedisConnectionError:
+        raise ValidationError(_("AI support is not available right now!"))
 
     ai_response = ai_response["messages"][-1].content
 
