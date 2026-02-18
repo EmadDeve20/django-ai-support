a2d60d9595f1231f1a61cd8974dda2a36898eae3
EmadDeve20 <emaddeve20@gmail.com>
2026-02-18 21:02:16 +0330

    Remove another Exceptions. it's better to raise just an error

--- django_ai_support/backends.py
+++ django_ai_support/backends.py
@@ -1,13 +1,7 @@
-from redis.exceptions import ConnectionError as RedisConnectionError
-from psycopg2 import OperationalError as PostgresConnectionError
-from pymongo.errors import ConnectionFailure as MongodbConnectionError
-from pymongo.errors import ServerSelectionTimeoutError
-
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
 
 from langchain_core.messages import HumanMessage
-
 
 from .graphs import compiled_graph
 
@@ -32,19 +26,6 @@ def normal_chat_with_ai(user_message:str, thread_id:str) -> str:
 
     try:
         ai_response = compiled_graph.invoke({"messages": messages}, config=config)
-
-    except RedisConnectionError:
-        raise ValidationError(_("AI support is not available right now!"))
-
-    except PostgresConnectionError:
-        raise ValidationError(_("AI support is not available right now!"))
-
-    except MongodbConnectionError:
-        raise ValidationError(_("AI support is not available right now!"))
-
-    except ServerSelectionTimeoutError:
-        raise ValidationError(_("AI support is not available right now!"))
-
     except Exception:
         raise ValidationError(_("AI support is not available right now!"))
 
